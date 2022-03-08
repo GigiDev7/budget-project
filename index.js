cb1796b8a07b6466e22360f70e88fa265a32d55d
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-03-08 12:23:14 +0400

    added auth functionality on login, creating token

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 //routers
 const userRouter = require("./routes/user");
@@ -10,6 +12,7 @@ const categoryRouter = require("./routes/categories");
 const app = express();
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 
 //routes
@@ -20,7 +23,7 @@ app.use("/api/incomes", incomesRouter);
 app.use("/api/category", categoryRouter);
 
 //server
-const PORT = 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
--- services/loginUser.js
+++ services/loginUser.js
@@ -1,7 +1,20 @@
+const jwt = require("jsonwebtoken");
+
 const loginUser = async (userData) => {
   // make DB query
   // check if we have user in DB
   // write authentication  logic
+  // compare bcrypted passwords
+
+  const token = jwt.sign(
+    {
+      /* user id from mongo DB */
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES }
+  );
+
+  return { token /* user */ };
 };
 
 module.exports = loginUser;
