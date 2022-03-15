79516c10cdb7d97c5d0e978d7df7567b3ec0d3d5
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-03-15 17:14:15 +0400

    added registration and login functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,43 @@
-const { loginUser, registerUser } = require("../services/user");
+const {
+  loginUser,
+  registerUser,
+  createToken,
+  comparePasswords,
+} = require("../services/user");
 
 const login = async (req, res) => {
-  await loginUser();
-  res.status(200).send("logged in");
+  try {
+    const user = await loginUser(req.body);
+    //checking if user exists in DB
+    if (!user) {
+      return res.status(400).json({ message: "No user found with such email" });
+    }
+    console.log(user, req.body);
+    //checking if passwords are correct
+    const isPasswordCorrect = await comparePasswords(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
+
+    const token = createToken(user._id);
+    res.status(200).json({ email: user.email, id: user._id, token });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 };
 
 const register = async (req, res) => {
-  //logic for user registartion
-  await registerUser();
-  res.status(200).send("registered");
+  try {
+    await registerUser(req.body);
+    res.status(200).send("registered");
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 };
 
 module.exports = {
