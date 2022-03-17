153e089bbe0200e490963bf851ae6df5cfe0ee72
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-03-17 22:41:36 +0400

    added all unit tests to test user routes, chaged previous logic a little bit

--- user.test.js
+++ user.test.js
@@ -1,6 +1,7 @@
 const app = require("./index");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const User = require("./models/userSchema");
 
 describe("user", () => {
   beforeAll(async () => {
@@ -16,7 +17,7 @@ describe("user", () => {
     it("should login user", async () => {
       const response = await supertest(app)
         .post("/api/user/login")
-        .send({ email: "email@gmail.com", password: "123456" });
+        .send({ email: "test1@gmail.com", password: "123456" });
 
       expect(response.status).toBe(200);
       expect(response.body).toMatchObject({
@@ -42,13 +43,44 @@ describe("user", () => {
   });
 
   describe("POST Register: request is valid", () => {
+    beforeAll(async () => {
+      await User.deleteOne({ email: "test2@gmail.com" });
+    });
+
     it("should create new user", async () => {
       const response = await supertest(app)
         .post("/api/user/register")
-        .send({ email: "test1@gmail.com", password: "123456" });
+        .send({ email: "test2@gmail.com", password: "123456" });
 
       expect(response.status).toBe(200);
       expect(response.body.message).toBe("registered");
+    });
+  });
+
+  describe("POST Register: email is already registered", () => {
+    it("should return error message", async () => {
+      const response = await supertest(app)
+        .post("/api/user/register")
+        .send({ email: "test2@gmail.com", password: "123456" });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toMatchObject({
+        email: expect.any(String),
+      });
+    });
+  });
+
+  describe("POST Register: invalid email and password", () => {
+    it("should return error message", async () => {
+      const response = await supertest(app)
+        .post("/api/user/register")
+        .send({ email: "test2@gmail.", password: "12345" });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toMatchObject({
+        email: expect.any(String),
+        password: expect.any(String),
+      });
     });
   });
 });
