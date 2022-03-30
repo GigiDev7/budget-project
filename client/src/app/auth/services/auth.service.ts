ac81eb9fe58cea03569dc6ab9cf59389d7ca5e18
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-03-30 22:18:03 +0400

    added logic to add token to localstorage and also delete on logout

--- client/src/app/auth/services/auth.service.ts
+++ client/src/app/auth/services/auth.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  user = {};
-
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string) {
-    return this.http.post('http://localhost:5000/api/user/login', {
-      email,
-      password,
-    });
+    return this.http
+      .post('http://localhost:5000/api/user/login', {
+        email,
+        password,
+      })
+      .pipe(tap((res) => this.setToken(res)));
+  }
+
+  private setToken(data: any) {
+    localStorage.setItem('token', data.token);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
   }
 }
