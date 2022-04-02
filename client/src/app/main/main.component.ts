205815afa5232db6ca41a8d222a90c451a869470
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-04-02 20:25:52 +0400

    added logic to get transactions on initial render

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountModel } from '../account-card/account.model';
 import { AccountService } from '../account-card/services/account.service';
+import { TransactionService } from '../transaction-card/services/transaction.service';
+import { TransactionModel } from '../transaction-card/transaction.model';
 
 @Component({
   selector: 'app-main',
@@ -9,16 +11,27 @@ import { AccountService } from '../account-card/services/account.service';
 })
 export class MainComponent implements OnInit {
   public accounts!: AccountModel[];
+  public transactions!: TransactionModel[];
 
-  public trackBy(index: number, item: AccountModel) {
+  public trackBy(index: number, item: AccountModel | TransactionModel) {
     return item._id;
   }
 
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private transactionService: TransactionService
+  ) {}
 
   public ngOnInit(): void {
     this.accountService.getAccounts().subscribe({
       next: (data) => (this.accounts = data),
+      complete: () => {
+        this.transactionService
+          .getTransactions(this.accounts[1]._id)
+          .subscribe({
+            next: (data) => (this.transactions = data),
+          });
+      },
     });
   }
 }
--- client/src/app/transaction-card/transaction-card.component.ts
+++ client/src/app/transaction-card/transaction-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TransactionModel } from './transaction.model';
 
 @Component({
   selector: 'app-transaction-card',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./transaction-card.component.scss'],
 })
 export class TransactionCardComponent {
+  @Input() transaction!: TransactionModel;
+
   constructor() {}
 }
