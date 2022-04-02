8246f8cbdb3e3221c7f902a9fe87023447df93ad
Giorgi Vadachkoria <gigivadachkoria@gmail.com>
2022-04-02 13:34:36 +0400

    added logic to logout user when token expires

--- client/src/app/auth/auth.interceptor.ts
+++ client/src/app/auth/auth.interceptor.ts
@@ -8,16 +8,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserModel } from './auth.model';
 import { AuthService } from './services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    if (this.authService.isLoggedIn()) {
+    if (this.authService.isLoggedIn() && this.isTokenExpired()) {
+      this.router.navigateByUrl('login');
+      return next.handle(req);
+    } else if (this.authService.isLoggedIn()) {
       const user: UserModel = JSON.parse(localStorage.getItem('user')!);
       const cloned = req.clone({
         headers: req.headers.set('Authorization', `Bearer ${user.token}`),
@@ -25,5 +30,12 @@ export class AuthInterceptor implements HttpInterceptor {
       return next.handle(cloned);
     }
     return next.handle(req);
+  }
+
+  private isTokenExpired(): boolean {
+    const helper = new JwtHelperService();
+    const user: UserModel = JSON.parse(localStorage.getItem('user')!);
+    const isExpired = helper.isTokenExpired(user.token);
+    return isExpired;
   }
 }
